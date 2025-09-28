1226b6dca595a49e5da745fdec24559fddd99d7c
Kirupashree18 <vkirupa1802@gmail.com>
2025-09-28 10:57:28 +0530

    Create factory.java

---
+++ Design_patterns/factory.java
@@ -0,0 +1,39 @@
+// Product
+interface Shape {
+    void draw();
+}
+
+// Concrete Products
+class Circle implements Shape {
+    public void draw() {
+        System.out.println("Drawing a Circle.");
+    }
+}
+
+class Rectangle implements Shape {
+    public void draw() {
+        System.out.println("Drawing a Rectangle.");
+    }
+}
+
+// Factory
+class ShapeFactory {
+    public static Shape getShape(String type) {
+        switch (type.toLowerCase()) {
+            case "circle": return new Circle();
+            case "rectangle": return new Rectangle();
+            default: throw new IllegalArgumentException("Unknown shape " + type);
+        }
+    }
+}
+
+// Demo
+public class FactoryPatternDemo {
+    public static void main(String[] args) {
+        Shape s1 = ShapeFactory.getShape("circle");
+        s1.draw();
+
+        Shape s2 = ShapeFactory.getShape("rectangle");
+        s2.draw();
+    }
+}
